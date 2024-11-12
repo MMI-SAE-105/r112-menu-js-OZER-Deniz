0b33dcfece86a70e0d9814a15f272882561ff640
OZER-Deniz <deniz.ozer@edu.univ-fcomte.fr>
2024-11-12 15:24:18 +0100

    ouverture menu

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.menu-btn');
+const nav = document.querySelector('.menu');
+
+toggle.addEventListener("click", () => {
+    
+    nav.ariaHidden = false;
+    toggle.ariaExpanded = true;
+});
